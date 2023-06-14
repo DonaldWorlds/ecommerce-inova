a8e5006d2a9287b4640351df27f962b5939ae66c
donald <donaldwitherspoon21@yahoo.com>
2023-06-14 02:52:06 -0400

    Third commit

--- model.js
+++ model.js
@@ -1,69 +1,51 @@
-// Retrieve cart items from local storage
-let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
-let totalCost = localStorage.getItem("totalCost");
-
-
 // Update the cart display
 function updateCartDisplay() {
     let cartItemsContainer = document.getElementById("cartItems");
     cartItemsContainer.innerHTML = ""; // Clear the existing content
-
+  
+    // Retrieve the cart items from localStorage
+    let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  
     // Check if cartItems is not empty
     if (cartItems && cartItems.length > 0) {
-        cartItems.forEach(item => {
-            let row = document.createElement("tr");
-            let productNameCell = document.createElement("td");
-            let priceCell = document.createElement("td");
-            let quantityCell = document.createElement("td");
-
-            productNameCell.textContent = item.name;
-            priceCell.textContent = item.price;
-            quantityCell.textContent = item.quantity;
-
-            row.appendChild(productNameCell);
-            row.appendChild(priceCell);
-            row.appendChild(quantityCell);
-
-            cartItemsContainer.appendChild(row);
-        });
+      cartItems.forEach(item => {
+        let row = document.createElement("tr");
+        let productNameCell = document.createElement("td");
+        let priceCell = document.createElement("td");
+        let quantityCell = document.createElement("td");
+  
+        productNameCell.textContent = item.name;
+        priceCell.textContent = item.price;
+        quantityCell.textContent = item.quantity;
+  
+        row.appendChild(productNameCell);
+        row.appendChild(priceCell);
+        row.appendChild(quantityCell);
+  
+        cartItemsContainer.appendChild(row);
+      });
     } else {
-        // Cart is empty
-        let emptyRow = document.createElement("tr");
-        let emptyCell = document.createElement("td");
-        emptyCell.textContent = "No items in the cart";
-
-        emptyRow.appendChild(emptyCell);
-        cartItemsContainer.appendChild(emptyRow);
+      // Cart is empty
+      let emptyRow = document.createElement("tr");
+      let emptyCell = document.createElement("td");
+      emptyCell.textContent = "No items in the cart";
+  
+      emptyRow.appendChild(emptyCell);
+      cartItemsContainer.appendChild(emptyRow);
     }
-
+  
     let totalCost = localStorage.getItem("totalCost");
     if (totalCost) {
-        document.querySelector("#totalCostElement").textContent = "Total Cost: $" + totalCost;
+      document.querySelector("#totalCostElement").textContent = "Total Cost: $" + totalCost;
     } else {
-        document.querySelector("#totalCostElement").textContent = "";
+      document.querySelector("#totalCostElement").textContent = "";
     }
-}
-
-// Call the updateCartDisplay function initially to display the cart
-updateCartDisplay();
-
-// Add event listener for "Add to Cart" button
-// Add event listener for "Add to Cart" button
-let addToCartButton = document.getElementById("checkoutBtn");
-addToCartButton.addEventListener("click", function () {
-    // ...
-
-    // Add the item to the cart
-    cartItems.push(cartItem);
-
-    // Update the cart items in local storage
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-
-    // Update the cart display
-    updateCartDisplay();
-
-    // Retrieve the updated values from local storage
-    cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
-    totalCost = localStorage.getItem("totalCost");
-});
-
+  }
+  
+  // Call the updateCartDisplay function initially to display the cart
+  window.addEventListener("DOMContentLoaded", updateCartDisplay);
+  
+  // Update the cart display when the cart button is clicked
+  let cartButton = document.getElementById("checkoutBtn");
+  cartButton.addEventListener("click", updateCartDisplay);
+  
--- script.js
+++ script.js
@@ -1,137 +1,115 @@
-if (typeof Storage !== "undefined") {
-  console.log("it works");
-} else {
-  console.log("it dont work");
-}
-
-let cart = document.querySelectorAll(".addTocart");
-console.log(cart);
-
-let clearButton = document.querySelector(".clear");
-
-let products = [
-  {
-    name: "The user pack",
-    price: 29,
-  },
-  {
-    name: "The beginner pack",
-    price: 39,
-  },
-  {
-    name: "The Jelly pack ",
-    price: 49,
-  },
-  {
-    name: "The O'most there pack",
-    price: 59,
-  },
-  {
-    name: "The Go Getter pack",
-    price: 69,
-  },
-  {
-    name: "The Legend pack",
-    price: 79,
-  },
-  {
-    name: "The awesome pack",
-    price: 89,
-  },
-  {
-    name: "The lengendary pack",
-    price: 99,
-  },
-];
-
-for (let i = 0; i < cart.length; i++) {
-  cart[i].addEventListener("click", () => {
-    cartNumber(products[i]); //calling the cartNumber method
-    calculateTotalPrice(products[i]);
-    showClearButton();
-  });
-}
-
-function cartNumber(product) {
-  let productNumber = localStorage.getItem("cartNumbers");
-  productNumber = parseInt(productNumber);
-
-  /*The cartNumber function takes a (product parameter.)
-    It retrieves the value stored in the 'cartNumbers' key from the Local Storage using localStorage.getItem('cartNumbers').
-    The value is initially stored as a string.
-    The parseInt function is used to convert the retrieved string value to an integer and assign it to the productNumbers variable. **/
-
-  if (productNumber) {
-    localStorage.setItem("cartNumbers", productNumber + 1);
-    document.querySelector(".countedItems").textContent = productNumber + 1;
+document.addEventListener("DOMContentLoaded", function(){
+  if (typeof Storage !== "undefined") {
+    console.log("localStorage is supported");
   } else {
-    localStorage.setItem("cartNumbers", 1);
-    document.querySelector(".countedItems").textContent = 1;
+    console.log("localStorage is not supported");
   }
+  
+  let cart = document.querySelectorAll(".addTocart");
+  console.log(cart);
+  
+  let clearButton = document.querySelector(".clear");
+  
+  let products = [
+    {
+      name: "The user pack",
+      price: 29,
+    },
+    {
+      name: "The beginner pack",
+      price: 39,
+    },
+    {
+      name: "The Jelly pack ",
+      price: 49,
+    },
+    {
+      name: "The O'most there pack",
+      price: 59,
+    },
+    {
+      name: "The Go Getter pack",
+      price: 69,
+    },
+    {
+      name: "The Legend pack",
+      price: 79,
+    },
+    {
+      name: "The awesome pack",
+      price: 89,
+    },
+    {
+      name: "The lengendary pack",
+      price: 99,
+    },
+  ];
+  
+  for (let i = 0; i < cart.length; i++) {
+    cart[i].addEventListener("click", () => {
+      cartNumber(products[i]); //calling the cartNumber method
+      calculateTotalPrice(products[i]);
+      showClearButton();
+    });
+  }
+
+  function cartNumber(product) {
+  let productNumber = localStorage.getItem("cartNumbers");
+  let parsedProductNumber = parseInt(productNumber);
+
+  if (isNaN(parsedProductNumber)) {
+    // Set the initial cart number to 0 if it's not set or not a valid number
+    parsedProductNumber = 0;
+  }
+
+  let newProductNumber = parsedProductNumber + 1;
+  localStorage.setItem("cartNumbers", newProductNumber);
+  document.querySelector(".countedItems").textContent = newProductNumber;
 
   let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
   cartItems.push(product);
-
   localStorage.setItem("cartItems", JSON.stringify(cartItems));
 }
 
-/**
-   * The if statement checks if the productNumber variable is truthy. This condition is used to determine if there are already products in the cart.
+  
 
-  If there are products in the cart (the condition is true), the productNumbers value is incremented by 1 and stored back in the 'cartNumbers' key in the Local Storage using localStorage.setItem('cartNumbers', productNumbers + 1).
-
-  Additionally, the total count of items is updated on the webpage by setting the textContent of an element with the class '.countedNumbers' to productNumbers + 1.
-
-  If there are no products in the cart (the condition is false), the 'cartNumbers' key is set to 1, indicating the first item is added to the cart.
-
-  The count of items is updated on the webpage by setting the textContent of an element with the class '.countedItem' to 1.
-  */
-
-function calculateTotalPrice(product) {
-  let totalCost = localStorage.getItem("totalCost");
-  if (totalCost) {
-    totalCost = parseInt(totalCost);
+  function calculateTotalPrice(product) {
+    let totalCost = localStorage.getItem("totalCost");
+    totalCost = parseInt(totalCost) || 0;
     localStorage.setItem("totalCost", totalCost + product.price);
-  } else {
-    localStorage.setItem("totalCost", product.price);
+    document.querySelector(".totalCost").textContent = localStorage.getItem("totalCost");
   }
-  //update tht total cost value on the webpage
-  document.querySelector(".totalCost").textContent =
-    localStorage.getItem("totalCost");
-}
-
-function clearAll() {
-  localStorage.setItem("cartNumbers", 0);
-  localStorage.setItem("totalCost", 0);
-  document.querySelector(".countedItems").textContent = 0;
-  document.querySelector(".totalCost").textContent = 0;
-  localStorage.clear();
-  clearButton.style.display = "none";
-}
-
-document.querySelector(".clear").addEventListener("click", clearAll);
-
-function showClearButton() {
-  clearButton.style.display = "inline-block";
-}
-
-function retrieveCartData() {
-  let productNumber = localStorage.getItem("cartNumbers");
-  let totalCost = localStorage.getItem("totalCost");
-
-  if (productNumber) {
-    document.querySelector(".countedItems").textContent = productNumber;
+  
+  function clearAll() {
+    localStorage.removeItem("cartNumbers");
+    localStorage.removeItem("totalCost");
+    document.querySelector(".countedItems").textContent = 0;
+    document.querySelector(".totalCost").textContent = 0;
+    localStorage.removeItem("cartItems");
+    clearButton.style.display = "none";
+  }
+  
+  if (clearButton) {
+    clearButton.addEventListener("click", clearAll);
+  }
+  
+  function showClearButton() {
+    clearButton.style.display = "inline-block";
+  }
+  
+  function retrieveCartData() {
+    let productNumber = localStorage.getItem("cartNumbers");
+    let totalCost = localStorage.getItem("totalCost");
+  
+    if (productNumber) {
+      document.querySelector(".countedItems").textContent = productNumber;
+    }
+  
+    if (totalCost) {
+      document.querySelector(".totalCost").textContent = totalCost;
+    }
   }
 
-  if (totalCost) {
-    document.querySelector(".totalCost").textContent = totalCost;
-  }
-}
-
-// Add event listener to the Checkout button
-document.getElementById("checkoutBtn").addEventListener("click", function () {
-  // Redirect to the checkout page
-  window.location.href = "./checkout.html";
+  retrieveCartData();
+  
 });
-
-retrieveCartData();
